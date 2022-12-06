2e0e3729e7650f911c4c63de98950e4b2922e7a9
Thomas Ferré <t.ferre@digitsole.com>
2022-12-06 14:30:37 +0100

    Initial Commit

---
+++ BoxScore/BoxScore/BoxScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoxScoreApp.swift
+//  BoxScore
+//
+//  Created by TomF on 06/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct BoxScoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
