08b0d8e323456603e03aec6fedc2d3175795864e
Thomas Ferré <t.ferre@digitsole.com>
2022-12-13 17:52:53 +0100

    [#Extension] Color
    - Color extension created to access easily

---
+++ BoxScore/BoxScore/Extensions/Colors+Extension.swift
@@ -0,0 +1,17 @@
+//
+//  Colors+Extension.swift
+//  BoxScore
+//
+//  Created by TomF on 06/12/2022.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let background = Color("Background")
+    static let element = Color("Element")
+    static let subElement = Color("SubElement")
+    static let text = Color("Text")
+
+}
