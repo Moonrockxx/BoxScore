10aeb76c5db1da5c356a22cb41b4e053bc3817b8
Thomas Ferré <t.ferre@digitsole.com>
2022-12-26 22:00:23 +0100

    [#Settings] UI and User defaults

---
+++ BoxScore/BoxScore/View/NewGame/StatSheetView.swift
@@ -0,0 +1,20 @@
+//
+//  StatSheetView.swift
+//  BoxScore
+//
+//  Created by TomF on 23/12/2022.
+//
+
+import SwiftUI
+
+struct StatSheetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StatSheetView_Previews: PreviewProvider {
+    static var previews: some View {
+        StatSheetView()
+    }
+}
