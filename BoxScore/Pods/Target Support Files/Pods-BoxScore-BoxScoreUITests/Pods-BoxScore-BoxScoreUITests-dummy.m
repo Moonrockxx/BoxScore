832bda37dea721980f131d8669139292f505ffe3
Thomas Ferré <t.ferre@digitsole.com>
2023-02-01 18:45:35 +0100

    [#Other] Update project

--- BoxScore/Pods/Target Support Files/Pods-BoxScore-BoxScoreUITests/Pods-BoxScore-BoxScoreUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BoxScore_BoxScoreUITests : NSObject
-@end
-@implementation PodsDummy_Pods_BoxScore_BoxScoreUITests
-@end
--- BoxScore/Pods/Target Support Files/Pods-BoxScore/Pods-BoxScore-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BoxScore : NSObject
-@end
-@implementation PodsDummy_Pods_BoxScore
-@end
