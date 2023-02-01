0fa0581402f4bb02cd6c1b001efc739ae74643b0
Thomas Ferré <t.ferre@digitsole.com>
2023-02-01 19:16:44 +0100

    [#Other] Firebase added with SPM

--- BoxScore/BoxScoreApp.swift
+++ BoxScore/BoxScoreApp.swift
@@ -8,26 +8,17 @@
 import SwiftUI
 import FirebaseCore
 
-//class AppDelegate: NSObject, UIApplicationDelegate {
-//    //    override init() {
-//    //        super.init()
-//    //        FirebaseApp.configure()
-//    //    }
-//
-//    func application(_ application: UIApplication,
-//                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-//        //        FirebaseApp.configure()
-//        return true
-//    }
-//}
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
 
 @main
 struct BoxScoreApp: App {
-//    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
-    
-//    init() {
-//        FirebaseApp.configure()
-//    }
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
     var body: some Scene {
         WindowGroup {
